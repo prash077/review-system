30056237089a2ef13149f32ab176c2f264421443
unknown <rprashasthimmanuel@gmail.com>
2024-12-30 14:23:15 +0530

    views updated - reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,17 +1,17 @@
-# from django.shortcuts import render
-# from .forms import ReviewForm
-# from .models import Review
-# from ml.predict import predict_sentiment
+from django.shortcuts import render
+from .forms import ReviewForm
+from .models import Review
+from ml.predict import predict_sentiment
 
-# def index(request):
-#     if request.method == 'POST':
-#         form = ReviewForm(request.POST)
-#         if form.is_valid():
-#             review = form.save(commit=False)
-#             review.sentiment = predict_sentiment(review.text)
-#             review.save()
-#     else:
-#         form = ReviewForm()
+def index(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.sentiment = predict_sentiment(review.text)
+            review.save()
+    else:
+        form = ReviewForm()
 
-#     reviews = Review.objects.all()
-#     return render(request, 'index.html', {'form': form, 'reviews': reviews})
+    reviews = Review.objects.all()
+    return render(request, 'index.html', {'form': form, 'reviews': reviews})
